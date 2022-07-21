f713f5696ba3c02ce8407f8d4384671c09332acd
iamadityaraj04 <adityarohitraj@gmail.com>
2022-07-21 12:55:56 +0530

    node.js

--- urlModule/url.js
+++
@@ -1,7 +0,0 @@
-var url=require('url');
-var add='https://www.w3schools.com/nodejs/default.asp';
-var q=url.parse(add,true);
-
-console.log(q.host); //returns 'localhost:8080'
-console.log(q.pathname); //returns '/default.htm'
-console.log(q.search); //returns '?year=2017&month=february'
